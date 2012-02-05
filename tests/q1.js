935dbccf3ec335ba902ecc8ecae6f63c16a00f33
Matthew Phillips <matthew@phillipsoft.biz>
2012-02-04 19:27:41 -0500

    Need to figure out how I'm going to run tests on this thing.

--- tests/q1.js
+++ tests/q1.js
@@ -1,14 +1,8 @@
-var assert = require('assert'),
-    finger = require('../finger.js');
+var assert = require('assert');
 
 (function() {
 
  var query = 'matthew';
- var req = new finger.Request(query);
- var resp = new finger.Response(req);
- resp.reply(function(msg) {
-  console.log(msg);
-  assert.ok(true);
- });
+ // TODO How are we going to run our tests? 
 
 })();
